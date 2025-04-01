8e38c3cb4f7268e8b5b28fd765d9d184f4a65d6c
Raildom <Raildom05@gmail.com>
2025-04-01 16:59:14 -0300

    ler_string

--- Trabalho 1/ARQUIVOS_.C/utilitarios.c
+++ Trabalho 1/ARQUIVOS_.C/utilitarios.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
 #include "album.h"
+
+char *ler_string()
+{
+    char buffer[256], *string;
+
+    if (fgets(buffer, sizeof(buffer), stdin) == NULL)
+        string = NULL;
+    else
+    {
+        buffer[strcspn(buffer, "\n")] = 0;
+        string = strdup(buffer);
+    }
+    return string;
+}
