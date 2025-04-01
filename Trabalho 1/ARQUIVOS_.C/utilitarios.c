3a2a8320ef40bb02eb0eff2136c413dafe42985e
Raildom <Raildom05@gmail.com>
2025-04-01 16:54:13 -0300

    utilitarios

--- Trabalho 1/ARQUIVOS_.C/utilitarios.c
+++ Trabalho 1/ARQUIVOS_.C/utilitarios.c
@@ -3,3 +3,17 @@
 #include<string.h>
 
 #include "album.h"
+
+char *ler_string()
+{
+    char buffer[256], *string;
+
+    if (fgets(buffer, sizeof(buffer), stdin) == NULL)
+        string = NULL;
+    else
+    {
+        buffer[strcspn(buffer, "\n")] = 0;
+        string = strdup(buffer);
+    }
+    return string;
+}
