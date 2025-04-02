d28cae92893e021998b97ae17765f50bb53b8b1b
JM3L0 <jsousarufinoleal@gmail.com>
2025-04-02 08:58:58 -0300

    teste de cadastro

--- Trabalho 1/ARQUIVOS_.C/artista.c
+++ Trabalho 1/ARQUIVOS_.C/artista.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "artista.h"
-#include "album.h"
-#include "musica.h"
-#include "utilitarios.h"
+#include "../ARQUIVOS_.H/artista.h"
+#include "../ARQUIVOS_.H/album.h"
+#include "../ARQUIVOS_.H/musica.h"
+#include "../ARQUIVOS_.H/utilitarios.h"
+#include "../ARQUIVOS_.H/interface.h"
 
 ARTISTAS *aloca_no_artista(char *nome, char *tipo_artista, char *estilo_musical, int numero_albuns, ALBUNS *arv_albuns)
 {
@@ -20,5 +21,33 @@ ARTISTAS *aloca_no_artista(char *nome, char *tipo_artista, char *estilo_musical,
         novo->dir = NULL;
     }
     return novo;
+}
+
+void cadastrar_artista()
+{
+    char *nome, *tipo_artista, *estilo_musical;
+    int num_album;
+    // fazer ponteiro para albuns
+
+    printf("Digite o nome do Artista: ");
+    nome = ler_string();
+    printf("Digite o tipo do Artista: ");
+    tipo_artista = ler_string();
+    printf("Digite o estilo musical do Artista: ");
+    estilo_musical = ler_string();
+    printf("Digite o numero de albuns do Artista: ");
+    scanf("%d", &num_album);
+}
+int main(){
+    ARTISTAS *artista = aloca_no_artista("Artista1", "Tipo1", "Estilo1", 5, NULL);
+    if (artista != NULL)
+    {
+        printf("Artista cadastrado: %s\n", artista->nome_artista);
+        free(artista->nome_artista);
+        free(artista->tipo_artista);
+        free(artista->estilo_musical);
+        free(artista);
+    }
+    return 0;
 }
 
