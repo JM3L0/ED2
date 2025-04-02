7f62ecf355082057a1789a331d15c63f55ff5d79
Raildom <Raildom05@gmail.com>
2025-04-02 09:03:25 -0300

    aaaaa

--- Trabalho 1/ARQUIVOS_.H/artista.h
+++ Trabalho 1/ARQUIVOS_.H/artista.h
@@ -9,4 +9,7 @@ typedef struct ARTISTAS
     struct ARTISTAS *esq, *dir;
     struct ALBUNS *arv_albuns;
 } ARTISTAS;
+
+ARTISTAS *aloca_no_artista(char *nome, char *tipo_artista, char *estilo_musical, int numero_albuns, ALBUNS *arv_albuns)
+void cadastrar_artista();
 #endif
