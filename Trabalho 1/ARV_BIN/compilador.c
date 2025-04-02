1986c8b34b38362a0a634347078fae54bf27614f
JM3L0 <jsousarufinoleal@gmail.com>
2025-04-02 10:49:16 -0300

    haha la vem o erro

--- Trabalho 1/ARV_BIN/compilador.c
+++ Trabalho 1/ARV_BIN/compilador.c
@@ -3,16 +3,18 @@
 
 int main() {
     // Comando de compilação usando caminhos relativos
-    const char *comando = "gcc -o artista ARV_BIN/ARQUIVOS_.C/artista.c ARV_BIN/ARQUIVOS_.C/utilitarios.c ARV_BIN/ARQUIVOS_.C/album.c ARV_BIN/ARQUIVOS_.C/musica.c -IARV_BIN/ARQUIVOS_.H";
+    const char *comando = "gcc -o artista ../ARQUIVOS_.C/artista.c ../ARQUIVOS_.C/utilitarios.c ../ARQUIVOS_.C/album.c ../ARQUIVOS_.C/musica.c -I ARQUIVOS_.H";
 
     printf("Compilando o projeto...\n");
 
     // Executa o comando de compilação
     int retorno = system(comando);
+    
 
     // Verifica se a compilação foi bem-sucedida
     if (retorno == 0) {
         printf("Compilação concluída com sucesso!\n");
+        system("artista.exe");
     } else {
         printf("Erro durante a compilação. Código de retorno: %d\n", retorno);
         printf("Certifique-se de que está executando este programa no diretório principal do projeto.\n");
