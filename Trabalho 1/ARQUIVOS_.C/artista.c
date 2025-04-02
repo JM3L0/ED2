ccda030dc0a902100263cbb908fac5528d83da84
JM3L0 <jsousarufinoleal@gmail.com>
2025-04-02 08:35:08 -0300

    começando artista

--- Trabalho 1/ARQUIVOS_.C/artista.c
+++ Trabalho 1/ARQUIVOS_.C/artista.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include"artista.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "artista.h"
 #include "album.h"
-#include"musica.h"
-#include"utilitarios.h"
+#include "musica.h"
+#include "utilitarios.h"
 
--- Trabalho 1/ARQUIVOS_.C/interface.c
+++ Trabalho 1/ARQUIVOS_.C/interface.c
@@ -1,7 +1,33 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include"artista.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "artista.h"
 #include "album.h"
-#include"musica.h"
-#include"utilitarios.h"
+#include "musica.h"
+#include "utilitarios.h"
+
+void menu_dos_print()
+{
+    printf("[1]Cadastrar Artista\n");
+    printf("[2]Cadastrar Album\n");
+    printf("[3]Cadastrar Musica\n");
+    printf("[4]Cadastrar Playlist\n");
+    printf("Digite a opcao desejada: ");
+}
+
+void menu_geral()
+{
+    int opcao;
+    menu_dos_print();
+    scanf("%d", &opcao);
+    
+    switch (opcao)
+    {
+    case 1:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+}
