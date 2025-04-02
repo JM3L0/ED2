4c305c81e26c8f44ffdc4165f67be3436f1ce1a9
JM3L0 <jsousarufinoleal@gmail.com>
2025-04-02 11:06:55 -0300

    arquivo de compilador funcionando

--- Trabalho 1/ARV_BIN/compilador.c
+++ Trabalho 1/ARV_BIN/compilador.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
     // Comando de compilação usando caminhos relativos
     const char *comando = "gcc -o artista ../ARQUIVOS_.C/artista.c ../ARQUIVOS_.C/utilitarios.c ../ARQUIVOS_.C/album.c ../ARQUIVOS_.C/musica.c -I ARQUIVOS_.H";
 
@@ -9,15 +10,17 @@ int main() {
 
     // Executa o comando de compilação
     int retorno = system(comando);
-    
 
     // Verifica se a compilação foi bem-sucedida
-    if (retorno == 0) {
-        printf("Compilação concluída com sucesso!\n");
+    if (retorno == 0)
+    {
+        printf("Compilacao concluida com sucesso!\n");
         system("artista.exe");
-    } else {
-        printf("Erro durante a compilação. Código de retorno: %d\n", retorno);
-        printf("Certifique-se de que está executando este programa no diretório principal do projeto.\n");
+    }
+    else
+    {
+        printf("Erro durante a compilacao. Codigo de retorno: %d\n", retorno);
+        printf("Certifique-se de que esta executando este programa no diretorio principal do projeto.\n");
     }
 
     return 0;
