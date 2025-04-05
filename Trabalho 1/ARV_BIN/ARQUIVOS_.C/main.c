3692229d2da1cbab8511e9f21817b6251971ce5b
JM3L0 <jsousarufinoleal@gmail.com>
2025-04-05 14:16:37 -0300

    começo dos problemas

--- Trabalho 1/ARV_BIN/ARQUIVOS_.C/main.c
+++ Trabalho 1/ARV_BIN/ARQUIVOS_.C/main.c
@@ -9,7 +9,29 @@
 #include"../ARQUIVOS_.H/playlist.h"
 
 //TESTE SOCIAL
-int main (){
+int main()
+{
+    ARTISTAS *raiz, *no;
+    int inseriu;
+    raiz = NULL;
+    no = NULL;
 
+    no = cadastrar_artista();
+    inseriu = inserir_artista(&raiz, no);
+    no = cadastrar_artista();
+    inseriu = inserir_artista(&raiz, no);
+
+    if (raiz != NULL)
+    {
+        // imprimir_todos_os_dados_do_artista(raiz);
+        // imprimir_todos_artistas(raiz);
+        // imprimir_artista_tipo(raiz, "solo");
+        // imprimir_artista_estilo(raiz, "rock");
+        imprimir_artista_estilo_tipo(raiz, "rock", "solo");
+        free(raiz->nome_artista);
+        free(raiz->tipo_artista);
+        free(raiz->estilo_musical);
+        free(raiz);
+    }
     return 0;
 }
