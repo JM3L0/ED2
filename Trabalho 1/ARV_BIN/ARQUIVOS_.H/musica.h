5ad068de1af02ebd04ba47252b6741f6623adc62
Raildom <Raildom05@gmail.com>
2025-04-12 16:15:32 -0300

    ajustes na biblioteca de musicas

--- Trabalho 1/ARV_BIN/ARQUIVOS_.H/musica.h
+++ Trabalho 1/ARV_BIN/ARQUIVOS_.H/musica.h
@@ -6,4 +6,14 @@
 #include "utilitarios.h"
 #include "interface.h"
 
+MUSICAS *alocar_musica(char **titulo_musica, float duracao_musica);
+MUSICAS *cadastrar_musica(char **titulo_musica);
+int inserir_musica(MUSICAS **raiz, MUSICAS *no);
+MUSICAS *existe_musica(MUSICAS *raiz, char *titulo_musica);
+int imprimir_todas_as_musicas(MUSICAS *raiz);
+int imprime_dados_da_musica_album_artista(ARTISTAS *raiz_art, char *nome_musica);
+int imprime_dados_da_musica_album(ALBUNS *raiz_alb, char *nome_musica);
+int imprime_dados_da_musica_buscada(MUSICAS *raiz_mus, char *nome_musica);
+void limpar_no_musica(MUSICAS *no);
+void limpar_arv_musica(MUSICAS **raiz);
 #endif
