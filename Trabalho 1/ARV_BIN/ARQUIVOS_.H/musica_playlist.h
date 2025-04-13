874e5541212baba83b70cda0d632af48b4f969ab
Raildom <Raildom05@gmail.com>
2025-04-13 10:24:51 -0300

    protótipos das funções de playlist e musica_playlist inseridos em seus .h

--- Trabalho 1/ARV_BIN/ARQUIVOS_.H/musica_playlist.h
+++ Trabalho 1/ARV_BIN/ARQUIVOS_.H/musica_playlist.h
@@ -6,4 +6,8 @@
 #include "playlist.h"
 #include "musica.h"
 
+MUSICA_PLAYLIST *alocar_musica_playlist(char *titulo_musica, float duracao_musica, char *nome_artista, char *titulo_album);
+int inserir_musica_playlist(MUSICA_PLAYLIST **raiz, MUSICA_PLAYLIST *no);
+MUSICA_PLAYLIST *existe_musica_playlist(MUSICA_PLAYLIST *raiz, char *titulo_musica, char *artista_musica, char *album_musica);
+
 #endif
--- Trabalho 1/ARV_BIN/ARQUIVOS_.H/playlist.h
+++ Trabalho 1/ARV_BIN/ARQUIVOS_.H/playlist.h
@@ -6,4 +6,10 @@
 #include "utilitarios.h"
 //#include "interface.h"
 
+PLAYLIST *aloca_no_playlist(char *titulo_playlist);
+PLAYLIST *existe_playlist(PLAYLIST *raiz, char *titulo_playlist);
+PLAYLIST *cadastrar_playlist();
+int inserir_playlist(PLAYLIST **raiz, PLAYLIST *no);
+int imprimir_playlists(PLAYLIST *raiz);
+void limpar_no_playlist(PLAYLIST *no);
 #endif
