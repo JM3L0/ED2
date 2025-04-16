3d388b38a149773829b7431da3427132f7b76ab5
Raildom <Raildom05@gmail.com>
2025-04-15 21:00:15 -0300

    informações pre-definidas

--- Trabalho 1/ARV_BIN/ARQUIVOS_.C/main.c
+++ Trabalho 1/ARV_BIN/ARQUIVOS_.C/main.c
@@ -2,9 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include "../ARQUIVOS_.H/interface.h"
+#include "../ARQUIVOS_.H/artista.h"
+#include "../ARQUIVOS_.H/album.h"
+#include "../ARQUIVOS_.H/musica.h"
+#include "../ARQUIVOS_.H/playlist.h"
+#include "../ARQUIVOS_.H/musica_playlist.h"
 
-int main()
-{
+int main() {
     menu_geral();
     return 0;
 }
