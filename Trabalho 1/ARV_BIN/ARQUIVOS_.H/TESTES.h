c739942ac194c1aa0bdab08c3f89232fe42135a6
JM3L0 <jsousarufinoleal@gmail.com>
2025-04-17 17:59:10 -0300

    criação do arquivo de teste

---
+++ Trabalho 1/ARV_BIN/ARQUIVOS_.H/TESTES.h
@@ -0,0 +1,9 @@
+#ifndef DADOS_PREDEFINIDOS_H
+#define DADOS_PREDEFINIDOS_H
+
+#include "artista.h"
+#include "playlist.h"
+
+void inicializar_dados_predefinidos(ARTISTAS **raiz_artista, PLAYLIST **raiz_playlist);
+
+#endif
