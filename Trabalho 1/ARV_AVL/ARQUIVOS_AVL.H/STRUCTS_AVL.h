d2c69db8aaff96e6dcb716e10abd48af2ff38fe8
JM3L0 <jsousarufinoleal@gmail.com>
2025-04-18 11:13:55 -0300

    structs avl

--- Trabalho 1/ARV_AVL/ARQUIVOS_AVL.H/STRUCTS_AVL.h
+++ Trabalho 1/ARV_AVL/ARQUIVOS_AVL.H/STRUCTS_AVL.h
@@ -4,6 +4,7 @@ typedef struct MUSICAS
 {
     char *titulo_musica;
     float duracao_musica;
+    int altura_musica;
     struct MUSICAS *esq, *dir;
 } MUSICAS;
 
@@ -12,6 +13,7 @@ typedef struct ALBUNS
     char *titulo_album;
     int ano_lancamento;
     int quantidade_musicas;
+    int altura_album;
     struct ALBUNS *esq, *dir;
     struct MUSICAS *arv_musicas;
 } ALBUNS;
@@ -22,6 +24,7 @@ typedef struct ARTISTAS
     char *tipo_artista;
     char *estilo_musical;
     int numero_albuns;
+    int altura_artista;
     struct ARTISTAS *esq, *dir;
     struct ALBUNS *arv_albuns;
 } ARTISTAS;
@@ -30,6 +33,7 @@ typedef struct PLAYLIST
 {
     char *titulo_playlist;
     int quantidade_musicas_playlist;
+    int altura_playlist;
     struct PLAYLIST *esq, *dir;
     struct MUSICA_PLAYLIST *arv_musicas_playlist;
 } PLAYLIST;
@@ -40,6 +44,7 @@ typedef struct MUSICA_PLAYLIST
     float duracao_musica;
     char *artista_musica;
     char *album_musica;
+    int altura_musica_playlist;
     struct MUSICA_PLAYLIST *esq, *dir;
 } MUSICA_PLAYLIST;
 
